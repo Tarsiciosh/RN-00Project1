671bd4b942acb7b431c0b06ae0a5ff110bd0dbe5
Tar <tarsiciosh@gmail.com>
2020-06-18 14:59:44 -0300

    atom

--- App.js
+++ App.js
@@ -1,36 +1,96 @@
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Button, Vibration} from 'react-native';
+import Constants from 'expo-constants';
+import Counter from './Counter'
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+const REST_TIME = 300
+const STUDY_TIME = 1500
+
+export default class App extends React.Component {
+  
+  state = {
+    count: STUDY_TIME,
+    studyTime: STUDY_TIME,
+    restTime: REST_TIME,
+    shouldRun: false,
+    countReached : false,
+    studying : true,
+    vibrate : false,
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.dec, 1000)
+  }  
+
+  componentWillUnmount() {
+    clearInterval (this.interval)
+  }
+
+  startCounter = () => {
+    this.setState ({
+      shouldRun: true,
+    })
+  }
+
+  stopCounter = () => {
+    this.setState ({
+      shouldRun: false,
+    })
+  }
+
+  resetCounter = () => {
+    this.setState ({
+      count: 25,
+      studying: true,
+    })
+  }
+
+  vibrate = () => {
+    Vibration.vibrate ([500, 500, 500])
+  }
+
+  dec = () => {
+    this.setState (prevState => {
+      if (prevState.shouldRun) {
+        if (prevState.count > 0) {    
+          return {count: prevState.count - 1}
+        } else if (prevState.studying){
+          this.vibrate()
+          return ({
+            count: prevState.restTime,
+            studying: false,
+            vibrate: true,
+          })
+        } else if (!prevState.studying) {
+          this.vibrate()
+          return ({
+            count: prevState.studyTime,
+            studying: true,
+            vibrate: true,
+          })
+        }
+      }
+    })
+  }  
+          
+  render() {
+    return (
+      <View style={styles.container}>
+        <Button title="start" onPress={this.startCounter}/>
+        <Button title="stop" onPress={this.stopCounter}/>
+        <Button title="reset" onPress={this.resetCounter}/>
+        <Counter segCount={this.state.count} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    paddingTop: Constants.statusBarHeight, 
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+})
---
+++ Counter.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import PropTypes from 'prop-types'
+
+const Counter = props => {
+  const min = ("0" + Math.floor( props.segCount / 60)).slice(-2)
+  const seg = ("0" + (props.segCount % 60)).slice(-2)
+
+  return (
+    <Text style= {styles.text} > {min}:{seg} </Text>
+  )
+}
+
+Counter.prototypes= {
+  segCount: PropTypes.number,
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 50,
+    color: "darkblue",
+    fontFamily: "Verdana",
+  },
+})
+
+export default Counter
